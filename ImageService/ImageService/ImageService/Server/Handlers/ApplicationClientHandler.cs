b91c584497eaf411dd072364060e6351659571ed
shimon <shimoncohen655@gmail.com>
2018-06-12 15:58:10 -0400

    shimon's commit.

--- ImageService/ImageService/ImageService/Server/Handlers/ApplicationClientHandler.cs
+++ ImageService/ImageService/ImageService/Server/Handlers/ApplicationClientHandler.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using ImageService.Logging;
 using ImageService.Modal;
+using Infrastructure.Enums;
 using Infrastructure.Modal.Event;
 
 namespace ImageService.Server.Handlers
@@ -15,7 +18,8 @@ namespace ImageService.Server.Handlers
         #region Members
         private IImageServiceModal myModel;
         protected ILoggingService logging;
-        private object locker;
+        private NetworkStream stream;
+        private const int MAXREAD = 50000000;
         #endregion
 
         #region Properties
@@ -26,33 +30,42 @@ namespace ImageService.Server.Handlers
         {
             this.myModel = model;
             this.logging = logger;
-            locker = new object();
         }
 
         public void HandleClient(TcpClient client, object locker)
         {
             new Task(() =>
             {
-                //stream = client.GetStream();
-                //reader = new BinaryReader(stream);
-                //writer = new BinaryWriter(stream);
+                stream = client.GetStream();
 
                 // as long as the client is connected
                 while (client.Connected)
                 {
-                    byte[] commandLine;
+                    byte[] picture = new byte[MAXREAD];
+                    int numBytes;
                     try
                     {
                         // read a message from the client
-                        //commandLine = reader.ReadString();
-
-
+                        numBytes = stream.Read(picture, 0, MAXREAD);
+                        if (numBytes > 0)
+                        {
+                            // convert the stream of bytes to an image
+                            Image img = (Bitmap)((new ImageConverter()).ConvertFrom(picture));
+                            logging.Log("Recieved image from Application client", MessageTypeEnum.INFO);
+                            ServiceInfo info = ServiceInfo.CreateServiceInfo();
+                            // save the image
+                            img.Save(info.Handlers[0]);
+                            logging.Log("Saved image from Application client", MessageTypeEnum.INFO);
+                        }
+                        // initialize byte array
+                        picture.Initialize();
                     }
                     catch (Exception e)
                     {
+                        logging.Log("Error reading from Application client. Exiting client handler",
+                            MessageTypeEnum.FAIL);
                         break;
                     }
-                    bool result;
                 }
             }).Start();
         }
